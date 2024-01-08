d8917eab928c78e91b0cf2315b6467f63cf9be02
Tejaswini1581 <154863599+Tejaswini1581@users.noreply.github.com>
2024-01-08 12:28:53 +0530

    Delete Bank directory

--- Bank/src/main/java/com/techwave/Bank/models/repositories/ProduceIDRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techwave.Bank.models.repositories;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.techwave.Bank.models.pojo.ProduceID;
-@Repository
-public interface ProduceIDRepository extends CrudRepository<ProduceID, String> {
-
-}
