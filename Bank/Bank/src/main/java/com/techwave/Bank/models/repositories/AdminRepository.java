ab65ce4c70a98178db68ae9af555af4d33c14d4d
Tejaswini1581 <154863599+Tejaswini1581@users.noreply.github.com>
2024-01-08 12:31:51 +0530

    Delete Bank directory

--- Bank/Bank/src/main/java/com/techwave/Bank/models/repositories/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techwave.Bank.models.repositories;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.techwave.Bank.models.pojo.Admin;
-@Repository
-public interface AdminRepository extends CrudRepository<Admin, String> {
-
-}
